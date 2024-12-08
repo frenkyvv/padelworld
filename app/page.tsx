4d8eaf76c6de121d34998dcb33dd196a0b82524b
frenkyvv <frenky98@gmail.com>
2024-12-08 16:44:51 -0600

    Se modifica el Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import '../app/styles.css';
-import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ResetDatos from "../app/componentes/ResetDatos";
 import TablaTodosJugadores from "../app/componentes/TablaTodosJugadores"; // Importar el componente correcto
 
 export default function Home() {
